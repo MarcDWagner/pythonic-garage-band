b127afdc5b1b091d2ac83d43c186f1989aa7e2a0
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-23 09:20:27 -0700

    added get_instrument method, all 3 derived classes pass get_instrument tests

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -11,37 +11,46 @@ class Musician:
 
 # Derived class
 class Guitarist(Musician):
-    def __init__(self, name="unknown"):
+    def __init__(self, name="unknown", instrument="guitar"):
         self.name = name
+        self.instrument = instrument
 
     def __str__(self):
-        return f"My name is {self.name} and I play guitar"
+        return f"My name is {self.name} and I play {self.instrument}"
 
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
 
+    def get_instrument(self):
+        return self.instrument
 
 # Derived class
 class Drummer(Musician):
-    def __init__(self, name):
+    def __init__(self, name,instrument="drums"):
         self.name = name
+        self.instrument = instrument
 
     def __str__(self):
-        return f"My name is {self.name} and I play drums"
+        return f"My name is {self.name} and I play {self.instrument}"
 
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
 
+    def get_instrument(self):
+        return self.instrument
+
 
 # Derived class
 class Bassist(Musician):
-    def __init__(self, name):
+    def __init__(self, name, instrument="bass"):
         self.name = name
+        self.instrument = instrument
 
     def __str__(self):
-        return f"My name is {self.name} and I play bass"
+        return f"My name is {self.name} and I play {self.instrument}"
 
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
 
-
+    def get_instrument(self):
+        return self.instrument
