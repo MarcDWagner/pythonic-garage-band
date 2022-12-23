b9a43502769fc314053d5481bfcc02a2209fa469
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-23 08:36:26 -0700

    up f in return string for guitarist name, passed first test

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -2,25 +2,29 @@ class Band:
     def __init__(self, musicians=None):
         self.musicians = musicians or []
 
+
+# base class
 class Musician:
     def __init__(self, name):
         self.name = name
 
 
+# Derived class
 class Guitarist(Musician):
-    def __init__(self, name):
+    def __init__(self, name="unknown"):
         self.name = name
 
-        def __str__(self):
-            return "My name is {self.name} and I play guitar"
+    def __str__(self):
+         return f"My name is {self.name} and I play guitar"
 
 
+# Derived class
 class Bassist(Musician):
     def __init__(self, name):
         self.name = name
 
 
-
+# Derived class
 class Drummer(Musician):
     def __init__(self, name):
         self.name = name
